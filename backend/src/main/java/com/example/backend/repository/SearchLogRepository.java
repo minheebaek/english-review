0278a007a0810066cee2519981bf01f89d0cf353
minhee <bsmq100@gmail.com>
2023-11-23 21:46:53 +0900

    fix:SearchLog Repository 에러고침

--- backend/src/main/java/com/example/backend/repository/SearchLogRepository.java
+++ backend/src/main/java/com/example/backend/repository/SearchLogRepository.java
@@ -1,6 +1,9 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.SearchLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SearchLogRepository extends JpaRepository<SearchLogRepository, Integer> {
+@Repository
+public interface SearchLogRepository extends JpaRepository<SearchLogEntity, Integer> {
 }
