720812add8d686ba30d99ade10a2963f47d476b7
minhee <bsmq100@gmail.com>
2023-11-28 05:34:30 +0900

    style:NU 코드 400->401 위치 변경

--- backend/src/main/java/com/example/backend/common/ResponseCode.java
+++ backend/src/main/java/com/example/backend/common/ResponseCode.java
@@ -6,12 +6,12 @@ public interface ResponseCode {
      //HTTP Status 400
      String VALIDATION_FAILED = "VF";
      String DUPLICATE_EMAIL = "DE";
-     String NOT_EXISTED_USER = "NU";
      String NOT_EXISTED_BOARD = "NB";
 
      //HTTP Status 401
      String SIGN_IN_FAIL = "SF";
      String AUTHORIZATION_FAIL = "AF";
+     String NOT_EXISTED_USER = "NU";
 
      //HTTP Status 403
      String NO_PERMISSION = "NP";
